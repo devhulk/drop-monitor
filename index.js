e738883bb2a42b847dc566eac0ee30e938167fb5
devhulk <gry2big93@gmail.com>
2021-11-14 14:49:41 -0500

    initial req

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const walletName = "test2"
 
 let options = {
     address : "addr_test1vprnpeaw6h2yhcjl0m7pcs6235utpz7kh3vzxhqxd8gka8g4s66y9",
+    mintWalletAddr : "addr_test1vprnpeaw6h2yhcjl0m7pcs6235utpz7kh3vzxhqxd8gka8g4s66y9",
     walletName : "testTwo",
     config: "testnet",
     metadata: {
@@ -39,7 +40,7 @@ let request = JSON.stringify(options)
 //                     console.log(error.toJSON())
 //                   });
 
-axios.post('http://localhost:3572/v1/cardano/address/mints',options ,{ headers: {'Content-Type': 'application/json'}})
+axios.post('http://localhost:3572/v1/cardano/address/mints', request,{ headers: {'Content-Type': 'application/json'}})
                 .then(response => {
                     console.log(response.data)
                 })
