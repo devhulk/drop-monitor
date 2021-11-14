0e88661f587c4b73d9cf3ba187a1d665b637b15e
devhulk <gry2big93@gmail.com>
2021-11-14 14:22:39 -0500

    initial req

--- index.js
+++ index.js
@@ -31,25 +31,20 @@ let options = {
 let request = JSON.stringify(options)
 
 
-axios.post('http://localhost:3572/v1/cardano/mint/asset',request ,{ headers: {'Content-Type': 'application/json'}})
+// axios.post('http://localhost:3572/v1/cardano/mint/asset',request ,{ headers: {'Content-Type': 'application/json'}})
+//                 .then(response => {
+//                     console.log(response.data)
+//                 })
+//                 .catch((error) => {
+//                     console.log(error.toJSON())
+//                   });
+
+axios.get('http://localhost:3572/v1/cardano/mints')
                 .then(response => {
                     console.log(response.data)
                 })
                 .catch((error) => {
-                    // if (error.response) {
-                    //   // Request made and server responded
-                    //   console.log(error.response.data);
-                    //   console.log(error.response.status);
-                    //   console.log(error.response.headers);
-                    // } else if (error.request) {
-                    //   // The request was made but no response was received
-                    //   console.log(error.request);
-                    // } else {
-                    //   // Something happened in setting up the request that triggered an Error
-                    //   console.log('Error', error.message);
-                    // }
                     console.log(error.toJSON())
                   });
-
 
 
