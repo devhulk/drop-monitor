f865fa27e59b3f5e515cd07ca497b9d6e5f44d1e
devhulk <gry2big93@gmail.com>
2021-11-15 16:26:57 -0500

    drop day

--- src/options.js
+++ src/options.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 let options = (walletName = "testOne", metadata = {}, payment = {}, mint = {}) => {
     let config = {
-        address : process.env.TEST_MINT_WALLET,
-        mintWalletAddr : process.env.TEST_MINT_WALLET,
+        address : process.env.MINT_WALLET,
+        mintWalletAddr : process.env.MINT_WALLET,
         walletName : walletName,
         config: process.env.NODE_CONFIG,
         metadata,
