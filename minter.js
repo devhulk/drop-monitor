b8547b7e5758b2fee7b97343a64895c5aa574656
devhulk <gry2big93@gmail.com>
2021-11-15 01:53:43 -0500

    mint script

--- minter.js
+++ minter.js
@@ -2,7 +2,7 @@ const { default: axios } = require("axios")
 import options from "./src/options"
 import nft from './test/nft.json'
 
-let getRandomNFT = JSON.parse(nft)
+let getRandomNFT = nft
 
 let metadata = getRandomNFT
 let baseConfig = options("testTwo", metadata)
@@ -10,9 +10,9 @@ console.log(baseConfig)
 
 let mint = (data) => {
     let promise = new Promise((resolve, reject) => {
-        axios.post('http://localhost:3572/v1/cardano/mint/asset', JSON.stringify(data), {headers: {'Content-Type': 'application/json'}})
+        axios.post('http://localhost:3572/v1/cardano/mint/asset', data, {headers: {'Content-Type': 'application/json'}})
         .then((response) => {
-            resolve(JSON.stringify(response.data))
+            resolve(response.data)
         })
         .catch(e => reject(e))
     })
