5ea5eaa93b4f391c622bc04859545b700e8f3858
devhulk <gry2big93@gmail.com>
2021-11-15 03:38:56 -0500

    mint script

--- src/options.js
+++ src/options.js
@@ -1,5 +1,5 @@
 let options = (walletName = "testOne", metadata = {}, payment = {}, mint = {}) => {
-    let data = {
+    return {
         address : "addr_test1vprnpeaw6h2yhcjl0m7pcs6235utpz7kh3vzxhqxd8gka8g4s66y9",
         mintWalletAddr : "addr_test1vprnpeaw6h2yhcjl0m7pcs6235utpz7kh3vzxhqxd8gka8g4s66y9",
         walletName : walletName,
@@ -9,7 +9,6 @@ let options = (walletName = "testOne", metadata = {}, payment = {}, mint = {}) =
         payment
     }
 
-    return data
 }
 
 // address : "addr_test1vprnpeaw6h2yhcjl0m7pcs6235utpz7kh3vzxhqxd8gka8g4s66y9",
