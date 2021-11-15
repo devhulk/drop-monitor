f2db20cb74a204e620ce9e22803f797dbc38176b
devhulk <gry2big93@gmail.com>
2021-11-15 15:09:21 -0500

    drop day

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "apps": [
+    {
+      "name": "drop-monitor",
+      "script": "./dist/drop-monitor.js",
+      "error_file": "../log/drop-error.log",
+      "out_file": "../log/drop-access.log",
+      "merge_logs": true,
+      "cwd": "./",
+      "instances": 0,
+      "exec_mode"  : "cluster_mode",
+      "env": {
+        "NODE_ENV": "production"
+      }
+    }
+  ]
+}
