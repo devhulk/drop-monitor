8598b675e94296856fe0c6a3eb7d7c92176e0de8
devhulk <gry2big93@gmail.com>
2021-11-15 19:09:19 -0500

    mint script

---
+++ staging/NINJAPUG33/minter.js
@@ -0,0 +1,32 @@
+const { default: axios } = require("axios")
+import options from "../../src/options"
+import nft from './NINJAPUG33.json'
+require('dotenv').config()
+let walletName = process.env.WALLET_NAME
+
+let getRandomNFT = nft
+
+let metadata = getRandomNFT
+metadata.amount = 1
+let baseConfig = options(walletName, metadata)
+console.log(baseConfig)
+
+let mint = (data) => {
+    let promise = new Promise((resolve, reject) => {
+        axios.post('http://localhost:3572/v1/cardano/mint/asset', data, {headers: {'Content-Type': 'application/json'}})
+        .then((response) => {
+            resolve(response.data)
+        })
+        .catch(e => reject(e))
+    })
+
+    return promise
+    
+}
+        
+
+mint(baseConfig)
+.then((response) => {
+    console.log(JSON.stringify(response))
+})
+.catch(e => console.log(e))
