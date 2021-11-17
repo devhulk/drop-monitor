30add2f214d0dd16e0c0f148a7b259547fabd327
devhulk <gry2big93@gmail.com>
2021-11-17 17:18:48 -0500

    mint script

--- drop-monitor.js
+++ drop-monitor.js
@@ -1,4 +1,5 @@
 const { default: axios } = require("axios")
+const { response } = require("express")
 const { default: options } = require("./src/options")
 require('dotenv').config()
 let walletName = process.env.WALLET_NAME
@@ -21,69 +22,70 @@ const hexToUtf8 = convert('hex', 'utf8')
 let sent = []
 let minted = []
 let paymentsReceived = []
-let invalidPayments = []
+let validUTXOs = []
 
 let dropMonitor = {} 
 
-let getCurrentUTXOs = axios.post('http://localhost:3572/v1/cardano/address/mints', request,{ headers: {'Content-Type': 'application/json'}})
+let getCurrentUTXOs = axios.post('http://localhost:3572/v1/cardano/address/utxos', request,{ headers: {'Content-Type': 'application/json'}})
                 .then(response => {
-                    const txs = JSON.parse(response.data)
-                    let myTXs = txs.map((utxo) => {
-                        if (utxo.amount.length >= 2) {
-                            let address = utxo.address
-                            let customerAddress = utxo.inputAddress
-                            let txInput = utxo.amount[1]
-                            let txHash = utxo["txHash"]
-                            let txOutput = utxo.amount[utxo.output_index]
-                            let txix = `${txHash}#${utxo.output_index}` 
-                            let policyID = txInput.unit.substring(0, 56)
-                            let tokenNameHex = txInput.unit.substring(56)
-                            let tokenName = hexToUtf8(tokenNameHex)
-                            let unspent = {output: txOutput.quantity, txix} 
+                    console.log(response.data)
+                    // const txs = JSON.parse(response.data)
+                    // let myTXs = txs.map((utxo) => {
+                    //     if (utxo.amount.length >= 2) {
+                    //         let address = utxo.address
+                    //         let customerAddress = utxo.inputAddress
+                    //         let txInput = utxo.amount[1]
+                    //         let txHash = utxo["txHash"]
+                    //         let txOutput = utxo.amount[utxo.output_index]
+                    //         let txix = `${txHash}#${utxo.output_index}` 
+                    //         let policyID = txInput.unit.substring(0, 56)
+                    //         let tokenNameHex = txInput.unit.substring(56)
+                    //         let tokenName = hexToUtf8(tokenNameHex)
+                    //         let unspent = {output: txOutput.quantity, txix} 
 
-                            let mint =  {address, inputAddress: customerAddress, recieved : txInput, unspent, policyID, tokenName, sentStatus: ""}
-                            mint["_id"] = txHash
+                    //         let mint =  {address, inputAddress: customerAddress, recieved : txInput, unspent, policyID, tokenName, sentStatus: ""}
+                    //         mint["_id"] = txHash
 
-                            if (address != utxo.inputAddress) {
-                                mint.sentStatus = true
-                                sent.push(mint)
-                            } else {
-                                mint.sentStatus = false
-                                minted.push(mint)
-                            }
-                            return mint
+                    //         if (address != utxo.inputAddress) {
+                    //             mint.sentStatus = true
+                    //             sent.push(mint)
+                    //         } else {
+                    //             mint.sentStatus = false
+                    //             minted.push(mint)
+                    //         }
+                    //         return mint
 
-                        } else {
+                    //     } else {
 
-                            let address = utxo.address
-                            let txHash = utxo["txHash"]
-                            let txInput = utxo.amount[0]
-                            let txix = `${txHash}#${utxo.output_index}` 
-                            let customerAddress = utxo.inputAddress
-                            let payment = txInput.quantity / 1000000
-                            let txOutput = utxo.amount[utxo.output_index]
-                            let unspent = {output: txOutput.quantity, txix} 
+                    //         let address = utxo.address
+                    //         let txHash = utxo["txHash"]
+                    //         let txInput = utxo.amount[0]
+                    //         let txix = `${txHash}#${utxo.output_index}` 
+                    //         let customerAddress = utxo.inputAddress
+                    //         let payment = txInput.quantity / 1000000
+                    //         let txOutput = utxo.amount[utxo.output_index]
+                    //         let unspent = {output: txOutput.quantity, txix} 
 
-                            if (payment >= 20) {
-                                let validPayment = {txHash, customerAddress, payment, recievingAddress: utxo.address, lovelace: txInput.quantity, txix, unspent}
-                                validPayment["_id"] = txHash
-                                paymentsReceived.push(validPayment)
-                                return validPayment
+                    //         if (payment >= 20) {
+                    //             let validPayment = {txHash, customerAddress, payment, recievingAddress: utxo.address, lovelace: txInput.quantity, txix, unspent}
+                    //             validPayment["_id"] = txHash
+                    //             paymentsReceived.push(validPayment)
+                    //             return validPayment
 
-                            } else {
-                                let invalidPayment = {txHash, address, customerAddress, status: "Payment below 20ADA", unspent}
-                                invalidPayment["_id"] = txHash
-                                invalidPayments.push(invalidPayment) 
-                                return invalidPayment 
-                            }
-                        } 
-                    })
-                    dropMonitor.payments = paymentsReceived 
-                    dropMonitor.minted =  minted
-                    dropMonitor.sent =  sent
-                    dropMonitor.failedPurchases = sent
-                    return myTXs
-                    //  return response.data
+                    //         } else {
+                    //             let invalidPayment = {txHash, address, customerAddress, status: "Payment below 20ADA", unspent}
+                    //             invalidPayment["_id"] = txHash
+                    //             validUTXOs.push(invalidPayment) 
+                    //             return invalidPayment 
+                    //         }
+                    //     } 
+                    // })
+                    // dropMonitor.payments = paymentsReceived 
+                    // dropMonitor.minted =  minted
+                    // dropMonitor.sent =  sent
+                    // dropMonitor.validUTXOs = sent
+                    // return myTXs
+                     return response.data
                 })
                 .catch(function (error) {
                     if (error.response) {
@@ -133,44 +135,53 @@ let updateSent = (sent) => {
 
 }
 
-let runResults = {} 
-let count = 0
+let updateUTXOS = (sent) => {
+    let promise = new Promise((resolve, reject) => {
+    axios.post('http://localhost:3572/v1/cardano/utxos/unused', JSON.stringify(sent),{ headers: {'Content-Type': 'application/json'}})
+        .then(response => {
+            resolve(response.data)
+        })
+        .catch(e => reject(e))
+    })
+
+    return promise
+
+}
 
 const run = () => {
-    count++
     
     getCurrentUTXOs
-        .then(() => {
-                updatePayments(dropMonitor.payments)
-                .then(results => {
-                    runResults.payments = results 
-                })
-                .catch((e) => console.log(e))
-        .then(() => {
-                updateMinted(dropMonitor.minted)
-                .then(results => {
-                runResults.minted = results 
-                })
-                .catch((e) => console.log(e))
-            })
-            .catch((e) => console.log(e))
-        .then(() => {
-                updateSent(dropMonitor.sent)
-                .then(results => {
-                    runResults.sent = results 
-                })
-                .catch((e) => console.log(e))
-            })
-            .catch((e) => console.log(e))
+        .then((utxos) => {
+            console.log(utxos)
+        //         updatePayments(dropMonitor.payments)
+        //         .then(results => {
+        //             runResults.payments = results 
+        //         })
+        //         .catch((e) => console.log(e))
+        // .then(() => {
+        //         updateMinted(dropMonitor.minted)
+        //         .then(results => {
+        //         runResults.minted = results 
+        //         })
+        //         .catch((e) => console.log(e))
+        //     })
+        //     .catch((e) => console.log(e))
+        // .then(() => {
+        //         updateSent(dropMonitor.sent)
+        //         .then(results => {
+        //             runResults.sent = results 
+        //         })
+        //         .catch((e) => console.log(e))
+        //     })
+        //     .catch((e) => console.log(e))
         })
         .catch((e) => e)
         // console.log(runResults)
 
 }
 
-let monitor = (interval) => {
+let monitor = () => {
     run()
-    console.log('running every 30 sec')
     // console.log('listening for on chain updates at ', interval, ' second intervals...')
     // setInterval(run, interval*1000)
 }
